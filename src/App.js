37bc7431f3dcec6a27dd073434723549bd133772
Melissa Mercado <melissa.a.merc@gmail.com>
2021-01-11 13:02:23 -0500

    added components and switch case as well installing reat-router-dom dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "/components/About";
+import Navbar from "/components/Nabvar";
+// import Fullpage from "./components/Fullpage";
+import Projects from "/components/Projects";
+import Skills from "/components/Skills";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={About} />
+        {/* <Route exact path="/Fullpage" component={Fullpage} /> */}
+        <Route exact path="/Navbar" component={Navbar} />
+        <Route exact path="/Projects" component={Projects} />
+        <Route exact path="/Skills" component={Skills} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,1 +1,23 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import NavLink from "../NavLink";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Navbar>
+        <NavLink path="/" classes={["navbar-brand"]}>
+          Melissa Mercado
+        </NavLink>
+
+        <nav className="ml-auto">
+          <NavLink path="/">About</NavLink>
+          <NavLink path="/Projects">Projects</NavLink>
+          <NavLink path="/Skills">Skills</NavLink>
+        </nav>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navbar;
